785609db8b7539dfbbe2c93ac0a6266d883e4c0a
yanyuliu92 <yliu33@chicagobooth.edu>
2021-04-15 10:21:36 +0000

    Add existing file

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -6,3 +6,14 @@ To get started with TDD, see the `README.md` file in your
 `ruby/sum-of-multiples` directory.
 =end
 
+class SumOfMultiples
+  def initialize(*numbers)
+    @numbers = numbers.select { |n| n > 0 }
+  end
+
+  def to(limit)
+    (1...limit)
+      .select { |n| @numbers.any? { |m| (n % m).zero? } }
+      .sum
+  end
+end
